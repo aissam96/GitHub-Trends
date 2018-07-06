32482ae4f2079a8a3d1317a31b0b62dc88a618a7
Aissam Erradouane <erradouane.aissam96@gmail.com>
2018-07-06 15:04:05 +0100

    Initial Commit

---
+++ GitHub Trends/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHub Trends
+//
+//  Created by Aissam Erradouane B on 7/6/18.
+//  Copyright © 2018 Aissam Erradouane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
